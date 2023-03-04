990d87452ccbf73dbec2511170dc6b48cb7cc0e4
config <wangyihanger@gmail.com>
2023-03-04 10:52:24 +0800

    fix(yum-install): add `-y`

--- main.py
+++ main.py
@@ -159,7 +159,7 @@ def setup():
     """
     commands = [
         "docker --version",
-        "sudo yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine",
+        "sudo yum remove -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine",
         "sudo yum install -y yum-utils",
         "sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo",
         "sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
